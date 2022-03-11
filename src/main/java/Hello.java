d8327463c561a6c84c10fdd18d4a3578afa9601e
huhjihye <floramiss@naver.com>
2022-03-11 15:40:47 +0900

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+    /*
+    *public static void main(String[] args)함수는 자바에서 가장 먼저실행되는 함수
+    *
+    * 즉, 자바 프로그램의 시작이 되는 함수이며, 반드시 Main 함수가 있어야지 자바가 실행가능함
+    * Main 함수가 없으면 실행이 안됨
+    * 인텔리제이는 타이핑할때 빨간색이 뜨면 오류라고 생각하면 된다.
+     */
+
+    public static void main(String[] args){
+
+        System.out.println("Hello!! My First Java Coding!!");
+    }
+}
