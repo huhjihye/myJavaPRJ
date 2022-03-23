88e3928b8b1d63cb004377051cedc3c17f58384a
huhjihye <floramiss@naver.com>
2022-03-23 10:34:00 +0900

    practice java Exam

---
+++ src/main/java/EX04_1.java
@@ -0,0 +1,22 @@
+public class EX04_1 {
+    public static void main(String[] args) {
+        int a, b = 5, c = 3;
+
+        a = b + c;
+        System.out.printf("%d+%d=%d \n", b, c, a);
+
+        a = b - c;
+        System.out.printf("%d-%d=%d \n", b, c, a);
+
+        a = b * c;
+        System.out.printf("%d*%d=%d\n", b, c, a);
+
+        a = b / c;
+        System.out.printf("%d/%d=%d \n", b, c, a);
+
+        a = b % c;
+        System.out.printf("%d%%%d=%d\n", b, c, a);
+
+
+    }
+}
